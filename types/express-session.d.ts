29df211b83d512cdd1ee6382d3535ed41e6b1136
PolicyBankAi <policy_bank_ai@policybankai.com>
2025-08-24 14:28:37 -0500

    Update express-session.d.ts

--- types/express-session.d.ts
+++ types/express-session.d.ts
@@ -1,13 +1,8 @@
-// types/express-session.d.ts
-
 import "express-session";
 
 declare module "express-session" {
   interface SessionData {
-    // Existing keys from your code
     demoUser?: any;
-
-    // Allow arbitrary custom keys without breaking type checks
     [key: string]: any;
   }
 }
