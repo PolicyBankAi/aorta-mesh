83ea3f46d4ae867daed5556031e2e694cabf80ad
PolicyBankAi <policy_bank_ai@policybankai.com>
2025-08-24 00:02:32 -0500

    Update toggle.tsx

--- client/src/components/ui/toggle.tsx
+++ client/src/components/ui/toggle.tsx
@@ -33,11 +33,10 @@ const Toggle = React.forwardRef<
 >(({ className, variant, size, ...props }, ref) => (
   <TogglePrimitive.Root
     ref={ref}
-    className={cn(toggleVariants({ variant, size, className }))}
+    className={cn(toggleVariants({ variant, size }), className)}
     {...props}
   />
 ))
-
 Toggle.displayName = TogglePrimitive.Root.displayName
 
 export { Toggle, toggleVariants }
