e941505d87e94c10c3357488a54c693dac21a7db
PolicyBankAi <policy_bank_ai@policybankai.com>
2025-08-23 23:47:45 -0500

    Update scroll-area.tsx

--- client/src/components/ui/scroll-area.tsx
+++ client/src/components/ui/scroll-area.tsx
@@ -29,18 +29,24 @@ const ScrollBar = React.forwardRef<
     ref={ref}
     orientation={orientation}
     className={cn(
-      "flex touch-none select-none transition-colors",
+      "flex touch-none select-none transition-colors duration-200",
       orientation === "vertical" &&
-        "h-full w-2.5 border-l border-l-transparent p-[1px]",
+        "h-full w-2.5 border-l border-transparent p-[1px]",
       orientation === "horizontal" &&
-        "h-2.5 flex-col border-t border-t-transparent p-[1px]",
+        "h-2.5 flex-col border-t border-transparent p-[1px]",
       className
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb
+      className={cn(
+        "relative flex-1 rounded-full bg-cyan-500/50 hover:bg-cyan-400 " +
+          "transition-colors duration-150 shadow-[0_0_6px_rgba(34,211,238,0.7)]"
+      )}
+    />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
 
 export { ScrollArea, ScrollBar }
+
